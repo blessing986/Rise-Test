409050747e7230c8c5f98b863958f5bc5e679012
Blessing U <ubiomorblessing@gmail.com>
2025-03-09 21:42:54 +0000

    chore:project setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h1 className="text-3xl font-bold underline">Hello world!</h1>,
+  },
+]);
+
+export default function App() {
+  return <RouterProvider router={router} />;
+}
