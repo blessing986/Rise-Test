4e57e89519d6c6afef75a0c888a997beed8f9ac1
Blessing U <ubiomorblessing@gmail.com>
2025-03-11 09:31:07 +0000

    chore: minor fix

--- src/components/BlogItem/BlogItem.tsx
+++ src/components/BlogItem/BlogItem.tsx
@@ -1,3 +1,5 @@
+import styles from "./BlogItem.module.css";
+
 interface BlogItemType {
   img: string;
   label: string;
@@ -6,44 +8,15 @@ interface BlogItemType {
 
 const BlogItem = ({ img, label, description }: BlogItemType) => {
   return (
-    <>
-      <div className="flex items-center lg:justify-center gap-[20px] sm:px-10 md:px-0">
-        <img src={img} alt={label} className="h-20 w-20" />
+    <div className={styles.blogItem}>
+      <img src={img} alt={label} className={styles.image} />
 
-        <div className="flex flex-col gap-[5px]">
-          <h6 className="text-[#222222] font-normal text-[15px] leading-[20px]">
-            {label}
-          </h6>
-          <p className="text-[#71879C] font-normal text-[16px] sm:text-[18px] leading-[100%]">
-            {description}
-          </p>
-        </div>
+      <div className={styles.textContainer}>
+        <h6 className={styles.label}>{label}</h6>
+        <p className={styles.description}>{description}</p>
       </div>
-    </>
+    </div>
   );
 };
 
 export default BlogItem;
-
-// import styles from "./BlogItem.module.css";
-
-// interface BlogItemType {
-//   img: string;
-//   label: string;
-//   description: string;
-// }
-
-// const BlogItem = ({ img, label, description }: BlogItemType) => {
-//   return (
-//     <div className={styles.blogItem}>
-//       <img src={img} alt={label} className={styles.image} />
-
-//       <div className={styles.textContainer}>
-//         <h6 className={styles.label}>{label}</h6>
-//         <p className={styles.description}>{description}</p>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default BlogItem;
